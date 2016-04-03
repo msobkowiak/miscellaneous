9bcde9d35d6e48163cf13aaf2f1f7491fa61bf4f
Monika Sobkowiak <monika.sobkowiak@gmail.com>
2016-04-03 23:08:36 +0100

    Pass paremeteres to cli

--- go/pirateby/searchParam.go
+++ go/pirateby/searchParam.go
@@ -4,19 +4,42 @@ import (
 	"strings"
 )
 
-var defaultHost = "https://pirateproxy.tv"
-var limit = 5
+var defaultUrl = "https://pirateproxy.tv"
 
 type SearchParams struct {
-	search      string
-	resultLimit int
-	host        string
+	search string
+	url    string
+}
+
+func (s *SearchParams) Create(params []string) {
+	s.search = params[0]
+
+	if len(params) > 1 {
+		s.url = createValidUlr(params[1])
+	} else {
+		s.url = defaultUrl
+	}
 }
 
 func fillInWhiteSpaces(s string) string {
 	return strings.Replace(s, " ", "%20", -1)
 }
 
-func (s *SearchParams) ConstructUrl(search string) string {
-	return s.host + "/search/" + fillInWhiteSpaces(search)
+func createValidUlr(urlStr string) string {
+
+	prefix := "http://"
+	if !strings.HasPrefix(urlStr, prefix) {
+		urlStr = prefix + urlStr
+	}
+
+	suffix := "/"
+	if strings.HasSuffix(urlStr, suffix) {
+		urlStr = urlStr[:len(urlStr)-len(suffix)]
+	}
+
+	return urlStr
+}
+
+func (s *SearchParams) ConstructUrl() string {
+	return s.url + "/search/" + fillInWhiteSpaces(s.search)
 }
